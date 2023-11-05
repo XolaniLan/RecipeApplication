64468af93814ff38f1fecb91c216888cc4f23257
Xolani <140137794+XolaniLan@users.noreply.github.com>
2023-11-05 20:17:43 +0200

    Hero Section Completed

--- RecipeApplication/assets/js/global.js
+++ RecipeApplication/assets/js/global.js
@@ -1,7 +1,13 @@
+"use strict";
 /**
- * @license MIT
- * @copyright 2023 codewithsadee
- * @author codewithsadee <mohammadsadee24@gmail.com>
+ * Add event on multiple elements
+ * @param {NodeList} $elements NodeList
+ * @param {String} eventType Event type string
+ * @param {Function} callback Callback function
  */
 
-"use strict";
+window.addEventListener = ($elements, eventType, callback) => {
+  for (const $element of $elements) {
+    $element.addEventListener(eventType, callback);
+  }
+}
--- RecipeApplication/assets/js/home.js
+++ RecipeApplication/assets/js/home.js
@@ -1,7 +1,37 @@
-/**
- * @license MIT
- * @copyright 2023 codewithsadee
- * @author codewithsadee <mohammadsadee24@gmail.com>
- */
-
 "use strict";
+
+// home page search
+
+const /** {NodeElement} */ $searchField = document.querySelector("[data-search-field]");
+const /** {NodeElement} */ $searchBtn = document.querySelector("[data-search-btn]");
+
+$searchBtn.addEventListener("click", function () {
+  if ($searchField.value) window.location = `/recipes.html?q=${$searchField.value}`;
+});
+
+// Submit when Enter key is pressed
+$searchField.addEventListener("keydown", e => {
+  if (e.key === "Enter") $searchBtn.click();
+});
+
+// Tab panel navigation
+const /** {NodeList} */ $tabBtns = document.querySelectorAll("[data-tab-btn]");
+const /** {NodeList} */ $tabPanels = document.querySelectorAll("[data-tab-panel]");
+
+let /** {NodeElement} */[$lastActiveTabPanel] = $tabPanels;
+let /** {NodeElement} */[$lastActiveTabBtn] = $tabBtns;
+
+addEventOnElements($tabBtns, "click", function () {
+  $lastActiveTabPanel.setAttribute("hidden", "");
+  $lastActiveTabBtn.setAttribute("aria-selected", false);
+  $lastActiveTabBtn.setAttribute("tabindex", -1);
+
+  const /** {NodeElement} */ $currentTabPanel = document.querySelector(`#${this.getAttribute("aria-controls")}`);
+  $currentTabPanel.removeAttribute("hidden");
+  this.setAttribute("aria-selected", true);
+  this.setAttribute("tabindex", 0);
+
+  $lastActiveTabPanel = $currentTabPanel;
+  $lastActiveTabBtn = this;
+});
+
