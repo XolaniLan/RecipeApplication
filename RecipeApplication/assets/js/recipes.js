88f52bf332d2d3d5d570a04fe72d006b48e70083
Xolani <140137794+XolaniLan@users.noreply.github.com>
2023-11-12 20:17:45 +0200

    Recipe Page Almost complete

--- RecipeApplication/assets/js/recipes.js
+++ RecipeApplication/assets/js/recipes.js
@@ -1,7 +1,90 @@
-/**
- * @license MIT
- * @copyright 2023 codewithsadee
- * @author codewithsadee <mohammadsadee24@gmail.com>
- */
-
 "use strict";
+
+// Imports
+
+import { fetchData } from "./api.js";
+import { $skeletonCard, cardQueries } from "./global.js"
+import { getTime } from "./module.js";
+
+// Accordion
+
+const /** {NodeList} */ $accordions = document.querySelectorAll("[data-accordion]");
+
+/**
+ * @param {*NodeElement} $element Accordion node
+ */
+const initAccordion = function ($element) {
+  const /** {NodeElement} */ $button = $element.querySelector("[data-accordion-btn]");
+  let isExpanded = false;
+
+  $button.addEventListener("click", function () {
+    isExpanded = isExpanded ? false : true;
+    this.setAttribute("aria-expanded", isExpanded);
+  });
+}
+
+for (const $accordion of $accordions) initAccordion($accordion);
+
+// Filter bar toggle for mobile screen
+
+const /** {NodeElement} */ $filterBar = document.querySelector("[data-filter-bar]");
+const /** {NodeElement} */ $filterTogglers = document.querySelectorAll("[data-filter-toggler]");
+const /** {NodeElement} */ $overlay = document.querySelector("[data-overlay]");
+
+addEventListener($filterTogglers, "click", function () {
+  $filterBar.classList.toggle("active");
+  $overlay.classList.toggle("active");
+  const bodyOverflow = document.body.style.overflow;
+  document.body.style.overlay = bodyOverflow === "hidden" ? "visible" : "hidden";
+});
+
+// Filter submit and clear
+
+const /** {NodeElement} */ $filterSubmit = document.querySelector("[data-filter-submit]");
+const /** {NodeElement} */ $filterClear = document.querySelector("[data-filter-clear]");
+const /** {NodeElement} */ $filterSearch = $filterBar.querySelector("input[type ='search']");
+
+$filterSubmit.addEventListener("click", function () {
+  const /** {NodeList} */ $filterCheckboxes = $filterBar.querySelectorAll("input:checked");
+  const /** {Array} */ queries = [];
+
+  if ($filterSearch.value) queries.push(["q", $filterSearch.value]);
+
+  if ($filterCheckboxes.length) {
+    for (const $checkbox of $filterCheckboxes) {
+      const /**{String} */ key = $checkbox.parentElement.parentElement.dataset.filter;
+      queries.push([key, $checkbox.value]);
+    }
+  }
+  window.location = queries.length ? `?${queries.joinj("&").replace(/,/g, "=")}` : "/recipes.html";
+});
+
+$filterSearch.addEventListener("keydown", e => {
+  if (e.key === "Enter") $filterSubmit.click();
+})
+
+$filterClear.addEventListener("click", function () {
+  const /** {NodeList} */ $filterCheckboxes = $filterBar.querySelectorAll("input:checked");
+
+  $filterCheckboxes?.forEach(elem => elem.checked = false);
+  $filterSearch.value &&= "";
+})
+
+const /** {String} */ queryStr = window.location.search.slice(1);
+const /** {Array} */ queries = queryStr && queryStr.split("&").map(i => i.split("="));
+const /** {NodeElement} */ $filterCount = document.querySelector("[data-filter-count]");
+
+if (queries.length) {
+  $filterCount.style.display = "block";
+  $filterCount.innerHTML = queries.length;
+} else {
+  $filterCount.style.display = "none";
+}
+
+queryStr && queryStr.split("&").map(i => {
+  if (i.split("=")[0] === "q") {
+    $filterBar.querySelector("input[type='search']").value = i.split("=")[1].replace(/%20/g, " ");
+  } else {
+    $filterBar.querySelector(`[value="${i.split("=")[1].replace(/%20/g, " ")}"]`).checked = true;
+  }
+})
